82ce92336fc42aa399863289bcbbd2307f4df1c9
Kami <2981229904@qq.com>
2020-09-05 02:43:09 +0800

    序列的基本操作

--- Chinese_zodiac.py
+++ Chinese_zodiac.py
@@ -2,6 +2,17 @@
 
 chineseZodiac = '鼠牛虎兔龙蛇马羊猴鸡狗猪'
 
-print (chineseZodiac[1:4]) #取包含第一到第三的字符，且不含第四个字符
+print(chineseZodiac[0:4])  # 切片操作符：取包含第一到第三的字符，且不含第四个字符
 
-print (chineseZodiac[-1]) #取包含倒数第一的字符
+print(chineseZodiac[-1])  # 取包含倒数第一的字符
+
+year = 2018
+print(year % 12)
+print(chineseZodiac[year % 12])
+
+print('狗' in chineseZodiac)  # 对象[not] in 序列
+
+print(chineseZodiac + 'abcde')  # 序列+序列
+
+print(chineseZodiac * 3)  # 序列*整数
+
